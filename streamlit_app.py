67b8735875b4d151a2061517cb3a534413a0d2a8
Alex Wilkinson <addwilkinson@hotmail.co.uk>
2022-03-08 17:36:21 +0000

    Editing the streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ h2 {
     color: #34ebc6;
     font-size: 20;
     text-shadow: 2px 2px 4px #000000;
+    text-align: center;
 }
 
 h3 {
@@ -26,9 +27,11 @@ body{
 }
 """
 
-
+# Page setup
 st.set_page_config(layout = "wide")
 st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
+
+# Display image and header
 col1, col2, col3, col4, col5 = st.columns(5)
 with col3:
     st.image('EFPO.png')
@@ -36,12 +39,35 @@ col1, col2, col3 = st.columns([0.5,2,0.5])
 with col2:
     st.header('Welcome to the Emotional Faces of Public Opinion!',anchor='h2')
 
+# Page selectors
 page = st.sidebar.selectbox('Select page',
   ['About','Find Other Perspectives'])
+
+# About page
 if page == 'About':
     # Display the About content here
     st.subheader('About this app')
-    st.write("Currently, people are stuck in echo chambers on social media, meaning that they are not always seeing all the sides of a story.  We developed a tool that allows people to break out of these echo chambers, giving them the opportunity to explore and understand different public opinions to what they see in day-to-day life. In doing so, we hope this will reduce polarisation due to exposure theory.")
+    st.write("Currently, people are stuck in echo chambers on social media,\
+            meaning that they are not always seeing all the sides of a story.\
+            We developed a tool that allows people to break out of these echo chambers,\
+            giving them the opportunity to explore and understand different public opinions \
+            to what they see in day-to-day life. In doing so, we hope this will reduce polarisation due to exposure theory.")
+    # guide = st.expander(label='Need help to run the app?  Click here!' )
+    # with guide:
+    #     st.subheader('How to use this app')
+    #     st.write('1️⃣ Naviagate to the Find Other Perspectives page on the side bar')
+    #     # Insert image here
+    #     # st.image('step1.png')
+    #     st.write('2️⃣ Expland the search terms container')
+    #     # Insert image here
+    #     st.write('3️⃣ Enter your search term, dates, and the number of tweets you would like to scrape (minimum value of 500 tweets).')
+    #     # Insert image here
+    #     st.write('4️⃣ Click on the Submit Search button.')
+    #     # Insert image here
+    #     st.write("5️⃣ RECOMMENDED BUT OPTIONAL: Remain calm while the models run and the data loads (it shouldn't take too long). 😌")
+
+
+# Find other perspectives page
 else:
     # Display the Search Twitter content here
     st.subheader('Find other perspectives',anchor='h3')
@@ -51,13 +77,18 @@ else:
         col1, col2, col3, col4 = st.columns([3,1,1,1.25])
         with col1:
             title = st.text_input('Enter search term here:')
+            # Connect to api
+
         with col2:
             start_date=st.date_input('Enter start date:')
+            # Connect to api
         with col3:
             end_date=st.date_input('Enter end date:')
+            # Connect to api
         with col4:
             num_tweets= st.number_input('Enter number of tweets',min_value=500, step=100)
-        if st.button('Submit search'):
+            # Connect to api
+        if st.button('Submit Search'):
             col1, col2, col3, = st.columns([1,2,1])
             with col2:
                 result = st.image('keep-calm-we-are-loading-data.png',caption='Data is laoding...')
