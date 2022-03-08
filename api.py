0314a15c8f7a411cc23edb0980e2845fc6f53a10
tingfungchan <croy314@gmail.com>
2022-03-08 19:49:55 +0800

    update the dockerfile

--- api.py
+++ api.py
@@ -6,6 +6,9 @@ from datetime import datetime
 import pytz
 from EFPO.the_almighty import *
 import ipdb
+import nltk
+nltk.download('stopwords')
+
 
 app = FastAPI()
 app.add_middleware(
@@ -20,6 +23,8 @@ app.add_middleware(
 #    return {"greeting": "Hello world"}
 #@app.get("/predict")
 def predict(search,date_beg,date_end,number):
+    date_beg = str(date_beg).replace('/','-')
+    date_end = str(date_end).replace('/','-')
     date_beg = datetime.strptime(date_beg, "%Y-%m-%d")
     date_end = datetime.strptime(date_end, "%Y-%m-%d")
     model=EFPO_Model(search,date_beg,date_end,int(number))
@@ -28,4 +33,4 @@ def predict(search,date_beg,date_end,number):
     model.top_tweet_sentiment
     vis = model.top2vec_visualisation()
     return model.df#{'DataFrame':model.df,
-            #'Visualisation': vis}
+            #'Visualisation': vis} (edited)
